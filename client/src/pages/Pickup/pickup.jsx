6651473a595ba9896cb602dce4c820d2ec3ae876
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-04-22 20:46:31 -0700

    insert styling from mockup project. Comment out pickup.css.

--- client/src/pages/Pickup/pickup.jsx
+++ client/src/pages/Pickup/pickup.jsx
@@ -1,21 +1,18 @@
-import { useQuery, useMutation } from '@apollo/client';
-import './pickup.css'
-import TimePicker from 'react-time-picker';
+import { useQuery, useMutation } from "@apollo/client";
+// import "./pickup.css";
+import TimePicker from "react-time-picker";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
-import './TimePicker.css';
-import 'react-clock/dist/Clock.css';
-import { ADD_PICKUP } from '../../utils/mutations';
-import auth from '../../utils/auth';
+import "./TimePicker.css";
+import "react-clock/dist/Clock.css";
+import { ADD_PICKUP } from "../../utils/mutations";
+import auth from "../../utils/auth";
 
 import { QUERY_ME } from "../../utils/queries";
 import "react-datepicker/dist/react-datepicker.css";
 // CSS Modules, react-datepicker-cssmodules.css
- import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-import dayjs from 'dayjs';
-
-
-
+import "react-datepicker/dist/react-datepicker-cssmodules.css";
+import dayjs from "dayjs";
 
 const Pickup = () => {
   const [addPickup] = useMutation(ADD_PICKUP);
@@ -26,26 +23,26 @@ const Pickup = () => {
   const lastPickup = pickups[pickups?.length - 1] || {};
 
   const [startDate, setStartDate] = useState(new Date());
-  const [time, setTime] = useState('10:00');
+  const [time, setTime] = useState("10:00");
 
   const [formData, setFormData] = useState({
-    Date: '',
-    Time: '',
-    Location: '',
-    Notes: ''
+    Date: "",
+    Time: "",
+    Location: "",
+    Notes: "",
   });
 
   async function handleSubmit(event) {
     event.preventDefault();
 
-    const location = document.getElementById('location').value;
-    const notes = document.getElementById('notes').value;
+    const location = document.getElementById("location").value;
+    const notes = document.getElementById("notes").value;
 
     setFormData({
       Date: startDate,
       Time: time,
       Location: location,
-      Notes: notes
+      Notes: notes,
     });
 
     console.log(formData);
@@ -56,32 +53,32 @@ const Pickup = () => {
         Date: startDate,
         Time: time,
         Location: location,
-        Notes: notes
+        Notes: notes,
       },
     });
     window.location.reload();
-
   }
 
   if (loading) {
-    return <h2>Loading</h2>
+    return <h2>Loading</h2>;
   }
 
-
-
   if (!auth.loggedIn()) {
-    return <h1>Please login to access this page</h1>
+    return <h1>Please login to access this page</h1>;
   }
 
   return (
     <div className="container">
-
       <form onSubmit={handleSubmit}>
-        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-
+        <label>Select a date:</label>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+        />
+        <label>Enter a time:</label>
         <TimePicker onChange={setTime} value={time} />
 
-        <label>Location: </label>
+        <label className="addressLbl">Enter address location:</label>
         <input type="text" name="Location" id="location" />
 
         <label> Notes: </label>
@@ -90,28 +87,21 @@ const Pickup = () => {
         <button type="submit">Submit</button>
       </form>
 
-      {
-        (lastPickup?.Date) ?
-          (
-            <div>
-              
-              <p>Date: {dayjs.unix(parseInt(lastPickup.Date/1000)).format("MMM/DD/YYYY")}</p>
-              <p>Time: {lastPickup.Time}</p>
-              <p>Location: {lastPickup.Location}</p>
-              <p>Notes: {lastPickup.Notes}</p>
-            </div>
-          ) :
-          (
-            <h1>No pickup requested</h1>
-          )
-      }
-
+      {lastPickup?.Date ? (
+        <div>
+          <p>
+            Date:{" "}
+            {dayjs.unix(parseInt(lastPickup.Date / 1000)).format("MMM/DD/YYYY")}
+          </p>
+          <p>Time: {lastPickup.Time}</p>
+          <p>Location: {lastPickup.Location}</p>
+          <p>Notes: {lastPickup.Notes}</p>
+        </div>
+      ) : (
+        <h1>No pickup requested</h1>
+      )}
     </div>
-
-
-
   );
-}
+};
 
 export default Pickup;
-
